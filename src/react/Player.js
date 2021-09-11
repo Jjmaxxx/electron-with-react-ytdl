ab94306b9a3060287a407725a626423b014193fa
Jjmaxxx <justlee888@gmail.com>
2021-09-11 03:57:20 -0700

    pause button, volume slider, and video playing by itself with no user interference with timestamps updating all works now, i;m so tired it's 4 am

--- src/react/Player.js
+++ src/react/Player.js
@@ -12,13 +12,21 @@ import playerTheme from './utils/playerTheme.js';
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 
+const video ="videos/a.mp3"
 class Player extends React.Component{
     constructor(props){
         super(props);
         this.play = this.play.bind(this);
         this.state = {
             playing: true,
+            volume:0.02,
+            videoTime:0,
+            rawVideoTime:0,
+            duration:0
         }
+    }
+    ref = player => {
+        this.player = player;
     }
     play=()=>{
         if(this.state.playing){
@@ -27,19 +35,45 @@ class Player extends React.Component{
             this.setState({playing:true});
         }
     }
+    videoProgress= (state)=>{
+        state.playedSeconds = Math.trunc(state.playedSeconds);
+        this.setState({rawVideoTime:state.playedSeconds});
+        if(((state.playedSeconds - (Math.floor(state.playedSeconds/60) *60)) < 10 && state.playedSeconds <60)){
+            this.setState({videoTime:Math.floor(state.playedSeconds/60)+ ":0" +Math.floor(state.playedSeconds)})
+        }else if(Math.floor(state.playedSeconds) < 60){
+            this.setState({videoTime:Math.floor(state.playedSeconds/60)+ ":" +Math.floor(state.playedSeconds)})
+        }else if(state.playedSeconds === 60){
+            this.setState({videoTime:"1:00"})
+        }
+        else if((state.playedSeconds - (Math.floor(state.playedSeconds/60) *60)) < 10 && state.playedSeconds >60){
+            this.setState({videoTime:Math.floor(state.playedSeconds/60)+ ":0" +Math.floor(state.playedSeconds- Math.floor(state.playedSeconds/60) * 60)})  
+        }else{
+            this.setState({videoTime:Math.floor(state.playedSeconds/60)+ ":" +Math.floor(state.playedSeconds - Math.floor(state.playedSeconds/60) * 60)})
+        }
+        // console.log(this.state.videoTime);
+    }
+    videoDuration=(duration)=>{
+        this.setState({duration:duration});
+        console.log(this.state.duration);
+        // console.log("minutes: " + Math.floor(duration/60) + "seconds: " + Math.floor(((Math.floor(duration/60)-duration/60) * 60)*-1)); 
+    }
     render(){
         const classes = styles;
-        const {playing} = this.state;
+        const {playing, volume} = this.state;
         return(
             <div>
                 <MuiThemeProvider theme={playerTheme}>
                     <CssBaseline/>
                     <ReactPlayer 
-                        url = "videos/b.mp4"
-                        playing="true"
-                        volume="0.02"
+                        ref={this.ref}
+                        url = {video}
+                        playing={playing}
+                        volume={volume}
+                        pip={true}
                         width= "250px"
                         height="200px"
+                        onProgress={this.videoProgress}
+                        onDuration={this.videoDuration}
                         style= {classes.video}
                     />
                     <Drawer 
@@ -83,7 +117,11 @@ class Player extends React.Component{
                                         <VolumeDown color = "secondary"/>
                                     </Grid>
                                     <Grid item xs>
-                                    <Slider aria-labelledby="continuous-slider" />
+                                    <Slider 
+                                        aria-labelledby="continuous-slider"
+                                        onChange= {(e,value)=>{this.setState({volume:value/1000})}}
+                                        defaultValue={volume*1000}
+                                    />
                                         </Grid>
                                     <Grid item>
                                         <VolumeUp color = "secondary"/>
@@ -92,13 +130,18 @@ class Player extends React.Component{
                         </div>
                         <Grid style={{width:"60%",marginLeft:"auto",marginRight:"auto",marginBottom:"10px"}} container spacing={2} >
                             <Grid item>
-                                <div style= {{color:"white",marginTop:"3px"}}>0:00</div>
+                                <div style= {{color:"white",marginTop:"3px"}}>{this.state.videoTime}</div>
                             </Grid>
                             <Grid item xs>
-                                <Slider aria-labelledby="continuous-slider" />
+                                <Slider
+                                    min={0}
+                                    max= {this.state.duration}
+                                    value={this.state.rawVideoTime}
+                                    aria-labelledby="continuous-slider" 
+                                />
                             </Grid>
                             <Grid item>
-                                <div style= {{color:"white",marginTop:"3px"}}>5:00</div>
+                                <div style= {{color:"white",marginTop:"3px"}}>{Math.floor(this.state.duration/60) + ":"+ Math.floor(((Math.floor(this.state.duration/60)-this.state.duration/60) * 60)*-1)}</div>
                             </Grid>
                         </Grid>
                     </Drawer>
